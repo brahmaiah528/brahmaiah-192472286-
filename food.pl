217b811f59d8b11fa7c8f71ddad052458573ad47
brahmaiah528 <brahmaiahjampala0@gmail.com>
2025-12-27 08:54:10 +0530

    Create food.pl

---
+++ food.pl
@@ -0,0 +1,13 @@
+% Database facts – food items only
+food(rice).
+food(bread).
+food(potato).
+food(milk).
+food(egg).
+food(chicken).
+food(fish).
+food(apple).
+food(banana).
+food(orange).
+food(oil).
+food(butter).
